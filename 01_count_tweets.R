5ed257174b1c57d00efe2c98a330435bd367dad2
cjbarrie <chrisjbarrie@googlemail.com>
2022-04-19 12:17:30 +0100

    Update counts slides

--- 01_count_tweets.R
+++ 01_count_tweets.R
@@ -81,3 +81,74 @@ tweetcounts <- count_all_tweets(
   granularity = "hour",
   n = 500
 )
+
+## Adding arguments (users)
+
+tweetcounts <- count_all_tweets(
+  query = "Hogmanay",
+  users = "edhogmanay",
+  start_tweets = "2019-12-27T00:00:00Z",
+  end_tweets = "2020-01-05T00:00:00Z",
+  bearer_token = get_bearer(),
+  granularity = "day",
+  n = 500
+)
+
+# saveRDS(tweetcounts, "data/edhogmanaycounts.rds")
+
+## Normalizing
+
+tweetcounts <- count_all_tweets(
+  query = "immigration",
+  start_tweets = "2015-12-27T00:00:00Z",
+  end_tweets = "2020-01-05T00:00:00Z",
+  bearer_token = get_bearer(),
+  granularity = "day",
+  n = 5000
+)
+
+# saveRDS(tweetcounts, "data/immigcounts.rds")
+tweetcounts <- readRDS("data/immigcounts.rds")
+
+baselinecounts <- count_all_tweets(
+  query = "therefore",
+  start_tweets = "2015-12-27T00:00:00Z",
+  end_tweets = "2020-01-05T00:00:00Z",
+  bearer_token = get_bearer(),
+  granularity = "day",
+  n = 5000
+)
+
+# saveRDS(baselinecounts, "data/baselinecounts.rds")
+baselinecounts <- readRDS("data/baselinecounts.rds")
+
+normalize_counts <- function(tweetcounts, baselinecounts) {
+  tweetcounts <- tweetcounts$tweet_count
+  baselinecounts <- baselinecounts$tweet_count
+  normalized_counts <- tweetcounts/baselinecounts
+  return(normalized_counts)
+}
+
+tweetcounts$normalized_count <-
+  normalize_counts(tweetcounts = tweetcounts,
+                   baselinecounts = baselinecounts)
+
+
+tweetcounts$day <-
+  parse_date_time(tweetcounts$start, 
+                  orders = "ymd HMS")
+
+tweetcounts %>% ggplot() +
+  geom_line(aes(day, tweet_count)) +
+  theme_tufte(base_family = "Helvetica") +
+  theme(legend.position = "none") + 
+  labs(title = "Immigration tweet counts",
+       x= "Day", y = "N. tweets")
+
+tweetcounts %>% ggplot() +
+  geom_line(aes(day, normalized_count)) +
+  theme_tufte(base_family = "Helvetica") +
+  theme(legend.position = "none") + 
+  labs(title = "Immigration tweet counts (normalized)",
+       x= "Day", y = "N. tweets")
+
