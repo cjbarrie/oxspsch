ac9cff233fcbc883a28e4b9360074d9c0c66db58
cjbarrie <chrisjbarrie@googlemail.com>
2022-04-22 10:17:56 +0300

    Update slides

--- demos.R
+++ demos.R
@@ -1,3 +1,12 @@
 library(academictwitteR)
 library(tidyverse)
 
+tweetsimmi <- get_all_tweets(
+  query = c("immigrant"), 
+  start_tweets = "2010-01-01T00:00:00Z",
+  end_tweets = "2010-03-31T00:00:00Z",
+  country = "US",
+  place = c("arizona OR nevada"),
+  bearer_token = get_bearer(),
+  n = 1000
+)
